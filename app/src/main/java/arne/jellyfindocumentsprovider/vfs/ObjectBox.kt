ea4ce88d1f7529fe74a7f9bdc7d43282e86ca9b6
Arne <arnesacnussem@gmail.com>
2026-06-05 20:17:12 +0300

    fix: exclude ObjectBox browser from release builds
    Use Class.forName reflection to start Admin only when
    io.objectbox.android.Admin is on the classpath (debug).
    Falls back silently in release builds.

--- app/src/main/java/arne/jellyfindocumentsprovider/vfs/ObjectBox.kt
+++ app/src/main/java/arne/jellyfindocumentsprovider/vfs/ObjectBox.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.util.Log
 import io.objectbox.Box
 import io.objectbox.BoxStore
-import io.objectbox.android.Admin
 
 
 object ObjectBox {
@@ -26,8 +25,12 @@ object ObjectBox {
         store = MyObjectBox.builder()
             .androidContext(context.applicationContext)
             .build()
-        val started = Admin(store).start(context)
-        Log.i("ObjectBoxAdmin", "Started: $started")
+        try {
+            val adminClass = Class.forName("io.objectbox.android.Admin")
+            val admin = adminClass.getConstructor(BoxStore::class.java).newInstance(store)
+            val started = adminClass.getMethod("start", Context::class.java).invoke(admin, context)
+            Log.i("ObjectBoxAdmin", "Started: $started")
+        } catch (_: ClassNotFoundException) { }
 
         server = store.boxFor(JellyfinServer::class.java)
         virtualFile = store.boxFor(VirtualFile::class.java)
